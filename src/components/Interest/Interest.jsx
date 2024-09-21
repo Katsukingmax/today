8ae3c29271fbe5b7923eab8dda542d3ac6c5ca98
Katsukingmax <yano.katsuki.160103@icloud.com>
2024-09-21 16:38:04 -0700

    Update Interest.jsx

--- src/components/Interest/Interest.jsx
+++ src/components/Interest/Interest.jsx
@@ -19,14 +19,49 @@ const Interest = () => {
     console.log('Interest data stored in local storage: ', intereData);
     alert('Your interest "${feeling}" has been saved locally!');
   };
+
+  
   
   return (
     <div>
       <h1>What are you interested in</h1>
-      <form onSubmit={handleSubmit}></>
-      
+      <form onSubmit={handleSubmit}>
+      <label>
+        <input type="radio"
+                value="Art"
+                checked={interest==='Art'}
+                onChange={handleChange}
+          />
+        Art
+      </label>
+        <label>
+        <input type="radio"
+                value="Sport"
+                checked={interest==='Sport'}
+                onChange={handleChange}
+          />
+        Sport
+      </label>
+        <label>
+        <input type="radio"
+                value="Food"
+                checked={interest==='Food'}
+                onChange={handleChange}
+          />
+        Food
+      </label>
+        <label>
+        <input type="radio"
+                value="Relax"
+                checked={interest==='Relax'}
+                onChange={handleChange}
+          />
+        Relax
+      </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Interest
