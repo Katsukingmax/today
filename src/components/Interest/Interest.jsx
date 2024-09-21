e4225f434635fc8f2987b14fcbe71aebfaa5cd1f
Katsukingmax <yano.katsuki.160103@icloud.com>
2024-09-21 16:42:58 -0700

    Update Interest.jsx

--- src/components/Interest/Interest.jsx
+++ src/components/Interest/Interest.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Interest'
+import React { useState } from 'react'
+import './Interest.css'
 
 const Interest = () => {
   const [interest, setInterest]=useState('');
