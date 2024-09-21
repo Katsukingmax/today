fb85230edf22664171234c4b08e8b5e71c5a57a3
Ricky Sasaoka <153718270+rickys223@users.noreply.github.com>
2024-09-21 16:28:50 -0700

    Change Time

--- src/components/Time/Time.jsx
+++ src/components/Time/Time.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './Time.css'
+import nextButtonImg from '../../assets/nextbutton.PNG'
+import { useNavigate } from 'react-router-dom'
 const Time = () => {
+  const navigate = useNavigate();
+
+  const goToInterestPage = () => {
+    navigate('interest');
+  }
+
   return (
     <div>
-      
+      <div>
+        <h1>How much time do you have?</h1>
+        <input
+          type="radio"
+          name="time"
+          value="lessThanAnHour"
+        />
+        Less than 1 hour
+        <input
+          type="radio"
+          name="time"
+          value="twoToFive"
+        />
+        Between 2 hours and 5 hours
+
+        <input
+          type="radio"
+          name="time"
+          value="fiveToEight"
+        />
+        Between 5 hours and 8 hours
+
+        <input
+          type="radio"
+          name="time"
+          value="moreThanEight"
+        />
+        More than 8 hours
+      </div>
+      <button>
+        <img src={nextButtonImg} alt="Go to next page" />
+      </button>
     </div>
   )
 }
