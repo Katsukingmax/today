c0c9f97ebd09b63364f1317eb96675310917eedb
anastasiawendy <anastasiawendyy@gmail.com>
2024-09-21 15:42:24 -0700

    Anything

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 import Feeling from './components/Feeling/Feeling.jsx';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      { <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,9 +19,28 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <h1>test</h1>
+
       <Feeling />
     </div>
+  );
+}
+
+export default App;
+*/ 
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/Home/Home'; 
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
