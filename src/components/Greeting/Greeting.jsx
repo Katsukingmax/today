a006840efc508aabeecd1b1d3febbe7a61ce23dc
Ricky Sasaoka <153718270+rickys223@users.noreply.github.com>
2024-09-21 17:52:18 -0700

    Greeting

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,9 +1,18 @@
-import React, {  } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Greeting = () => {
+  const [username, setUsername] = useState('');
+
+  useEffect (() => {
+    const storedUsername = localStorage.getItem('username');
+    if(storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
+
   return (
     <div>
-     
+      <h1>Hello, {username}!</h1>
     </div>
   );
 };
