6d5efbcfab9389ad77cc4b9aaf7efb2e7fd0e2af
anastasiawendy <80388338+anastasiawendy@users.noreply.github.com>
2024-09-22 00:31:02 -0700

    Update Greeting.jsx

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,18 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Greeting.css'; // Import the CSS file
 
 const Greeting = () => {
   const [username, setUsername] = useState('');
+  const navigate = useNavigate();
 
-  useEffect (() => {
+  useEffect(() => {
     const storedUsername = localStorage.getItem('username');
-    if(storedUsername) {
+    if (storedUsername) {
       setUsername(storedUsername);
     }
-  }, []);
+
+    const timer = setTimeout(() => {
+      navigate('/feeling'); // Navigate to Feeling component
+    }, 2000); // 3 seconds delay
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
 
   return (
-    <div>
-      <h1>Hello, {username}!</h1>
+    <div className="splash-container"> {/* Apply the CSS class */}
+      <div className = "greeting-message">hi, <span className = "greeting-name">{username || 'guest'}</span>!</div>
+      <p>let's start <span className = "greeting-today">today</span></p>
     </div>
   );
 };
