d0ce109d5cc46d5b690d9be6b2178689dac004da
anastasiawendy <80388338+anastasiawendy@users.noreply.github.com>
2024-09-22 00:28:30 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
-// /*import logo from './logo.svg';
-// import './App.css';
-// import Feeling from './components/Feeling/Feeling.jsx';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       { <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header> */}
-
-//       <Feeling />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/Home/Home';
+import Greeting from './components/Greeting/Greeting';
+import Feeling from './components/Feeling/Feeling';
 import Time from './components/Time/Time';
-import Home from './components/Home/Home';  // Import Home component
+import Interest from './components/Interest/Interest';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* Home Page */}
-        <Routes>
-          <Route path="/" element={<Home />} /> {/* Add Home component */}
-          <Route path="/time" element={<Time />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/greeting" element={<Greeting />} />
+        <Route path="/feeling" element={<Feeling />} /> 
+        <Route path="/time" element={<Time />} /> 
+        <Route path="/interest" element={<Interest />} /> 
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
