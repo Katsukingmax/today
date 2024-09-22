52f7e64ed33b9c578211c6220db2ac0f287eefbd
anastasiawendy <80388338+anastasiawendy@users.noreply.github.com>
2024-09-22 00:22:05 -0700

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="newsletter-section">
         <div className="newsletter-title">
-        unlock your perfect <span className="newsletter-today">today</span>
+        Unlock your perfect <span className="newsletter-today">today</span>
         </div>
         <div className="newsletter-signup">
           <input
@@ -14,14 +14,12 @@ const Footer = () => {
             placeholder="Enter your email"
             className="email-input"
           />
-          <button className="get-started-button">join!</button>
+          <button className="get-started-button">Join!</button>
         </div>
       </div>
       <nav className="footer-nav">
         <ul className="footer-links">
-          <li><a href="/home">back to home</a></li>
-          <li><a href="/login">login</a></li>
-          <li><a href="/signup">sign up</a></li>
+          <li><a href="/home">Back to home</a></li>
         </ul>
       </nav>
     </footer>
